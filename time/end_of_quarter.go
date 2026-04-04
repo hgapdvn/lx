d31377b9e04edd8d4a4c12fafcd0b39bd72932d7
Hai Nguyen <nthanhhai2909@gmail.com>
2026-04-04 17:07:33 +0700

    feat: simplify EndOfQuarter function using StartOfQuarter

--- time/end_of_quarter.go
+++ time/end_of_quarter.go
@@ -12,35 +12,7 @@ import "time"
 //	end := lxtime.EndOfQuarter(t)
 //	// end: 2026-06-30 23:59:59.999999999 +0000 UTC (Q2)
 func EndOfQuarter(t time.Time) time.Time {
-	// Extract year and month
-	year := t.Year()
-	month := t.Month()
-
-	// Determine the ending month of the quarter
-	var endMonth time.Month
-	switch {
-	case month >= time.January && month <= time.March:
-		endMonth = time.March
-	case month >= time.April && month <= time.June:
-		endMonth = time.June
-	case month >= time.July && month <= time.September:
-		endMonth = time.September
-	default: // October, November, December
-		endMonth = time.December
-	}
-
-	// Get the last day of the quarter's ending month
-	// by going to the first day of the next month and subtracting 1 day
-	nextMonth := endMonth + 1
-	if nextMonth > time.December {
-		nextMonth = time.January
-		year++
-	}
-
-	firstOfNextMonth := time.Date(year, nextMonth, 1, 0, 0, 0, 0, t.Location())
-	lastDayOfQuarter := firstOfNextMonth.AddDate(0, 0, -1)
-
-	// Return with time set to end of day (23:59:59.999999999)
-	return time.Date(lastDayOfQuarter.Year(), lastDayOfQuarter.Month(), lastDayOfQuarter.Day(),
-		23, 59, 59, 999999999, t.Location())
+	return StartOfQuarter(t).
+		AddDate(0, 3, 0).
+		Add(-time.Nanosecond)
 }
